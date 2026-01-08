8e851bc308b1568801453bde724e7399eed0d168
ItsAsadullah <angrycreation@gmail.com>
2026-01-09 02:29:49 +0600

    Fix router basename for GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
-import { UserProvider } from './context/UserContext'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <UserProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </UserProvider>
+    {/* basename যোগ করা হলো যাতে গিটহাব পেজে সঠিক পাথ পায় */}
+    <BrowserRouter basename="/TypingMaster">
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
 )
