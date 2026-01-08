f7eb9a6a16e91ad5cd92f91985c6d62081ab8e83
ItsAsadullah <angrycreation@gmail.com>
2026-01-09 02:44:06 +0600

    Switch to HashRouter for GitHub Pages reload fix

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { BrowserRouter } from 'react-router-dom'
+// BrowserRouter এর বদলে HashRouter ইম্পোর্ট করা হলো
+import { HashRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* basename যোগ করা হলো যাতে গিটহাব পেজে সঠিক পাথ পায় */}
-    <BrowserRouter basename="/TypingMaster">
+    {/* GitHub Pages এর জন্য HashRouter সবচেয়ে নিরাপদ */}
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 )
+
