e73095c0692fb29db7a8eeecaf6c259a8c2ff017
ItsAsadullah <angrycreation@gmail.com>
2026-01-09 02:37:41 +0600

    Fix index.html script path

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/TypingMaster/", // রিপোজিটরি নাম অনুযায়ী বেস পাথ
+  base: "/TypingMaster/", // রিপোজিটরির নাম হুবহু মিল থাকতে হবে
 })
